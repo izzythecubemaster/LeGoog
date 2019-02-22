7e00d672eef46950cc8a9b0e48699a7b2df31838
Israel Montoya <israel.montoya@ruralsource.com>
2019-02-22 11:15:29 -0700

    Stubbed out functions for each button and added functionality to I'm feeling lucky

--- fun.js
+++ fun.js
@@ -1,3 +1,31 @@
+function exposeLegoog() {
+    document.title ="Legoog";
+}
+
+function about() {
+    exposeLegoog();
+}
+
+function store() {
+    exposeLegoog();
+}
+
+function gmail() {
+    exposeLegoog();
+}
+
+function images() {
+    exposeLegoog();
+}
+
+function hamburger() {
+    exposeLegoog();
+}
+
+function signIn() {
+    exposeLegoog();
+}
+
 function search() {
     let searchString = $("#search-input").val();
     let splitString = searchString.split("");
@@ -5,4 +33,35 @@ function search() {
     let messedString = reverseArr.join("");
     $("#search-input").val(messedString);
     $("#search-button").addClass("hidden");
+
+    exposeLegoog();
+}
+
+function feelingLucky() {
+    if (!$('#html').hasClass("mutating-background")) {
+        $('#html').addClass("mutating-background");
+    }
+    $("#lucky-button").text("I'm not Feeling so Lucky");
+    $("#lucky-button").attr("style","color: #D00;");
+    exposeLegoog();
+}
+
+function advertising() {
+    exposeLegoog();
+}
+
+function business() {
+    exposeLegoog();
+}
+
+function privacy() {
+    exposeLegoog();
+}
+
+function terms() {
+    exposeLegoog();
+}
+
+function settings() {
+    exposeLegoog();
 }
